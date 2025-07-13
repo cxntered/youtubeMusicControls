8fddb35a1c5d2735ad5fe9317e175e2c34588bae
cxntered <cxntered@duck.com>
2025-07-13 16:06:16 +1000

    ♻️ refactor: properly format hours for those who listen to karaoke streams on youtube music for some reason

--- components/PlayerComponent.tsx
+++ components/PlayerComponent.tsx
@@ -21,8 +21,14 @@ import { SeekBar } from "./SeekBar";
 const cl = classNameFactory("vc-ytmusic-");
 
 function formatSeconds(seconds: number) {
-    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = Math.floor(seconds % 60);
+
+    if (hours > 0) {
+        return `${hours}:${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+    }
+
     return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
 }
 
