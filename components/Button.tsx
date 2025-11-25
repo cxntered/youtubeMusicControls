d56f3f2a30b9acb5d007c6026b570824376c5500
cxntered <cxntered@duck.com>
2025-11-25 14:56:57 +1100

    ♻️ refactor: extract button & icons into separate files

---
+++ components/Button.tsx
@@ -0,0 +1,18 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { cl } from "./PlayerComponent";
+
+export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+    return (
+        <button
+            className={cl("button")}
+            {...props}
+        >
+            {props.children}
+        </button>
+    );
+}
--- components/PlayerComponent.tsx
+++ components/PlayerComponent.tsx
@@ -19,9 +19,11 @@ import { ContextMenuApi, FluxDispatcher, Menu, React, useEffect, useState, useSt
 import { settings } from "..";
 import { SongInfo } from "../lib/types";
 import { YouTubeMusicStore } from "../lib/YouTubeMusicStore";
+import { Button } from "./Button";
+import { PauseButton, PlayButton, Repeat, Shuffle, SkipNext, SkipPrev } from "./Icons";
 import { SeekBar } from "./SeekBar";
 
-const cl = classNameFactory("vc-ytmusic-");
+export const cl = classNameFactory("vc-ytmusic-");
 
 function formatSeconds(seconds: number) {
     const hours = Math.floor(seconds / 3600);
@@ -33,45 +35,6 @@ function formatSeconds(seconds: number) {
     }
 
     return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
-}
-
-function Svg(path: string, label: string) {
-    return () => (
-        <svg
-            className={cl("button-icon", label)}
-            height="24"
-            width="24"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            aria-label={label}
-            focusable={false}
-        >
-            <path d={path} />
-        </svg>
-    );
-}
-
-/*
- * KraXen's icons :yesyes:
- * from https://fonts.google.com/icons?icon.style=Rounded&icon.set=Material+Icons
- * older material icon style, but still really good
- */
-const PlayButton = Svg("M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z", "play");
-const PauseButton = Svg("M8 19c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2v10c0 1.1.9 2 2 2zm6-12v10c0 1.1.9 2 2 2s2-.9 2-2V7c0-1.1-.9-2-2-2s-2 .9-2 2z", "pause");
-const SkipPrev = Svg("M7 6c.55 0 1 .45 1 1v10c0 .55-.45 1-1 1s-1-.45-1-1V7c0-.55.45-1 1-1zm3.66 6.82l5.77 4.07c.66.47 1.58-.01 1.58-.82V7.93c0-.81-.91-1.28-1.58-.82l-5.77 4.07c-.57.4-.57 1.24 0 1.64z", "previous");
-const SkipNext = Svg("M7.58 16.89l5.77-4.07c.56-.4.56-1.24 0-1.63L7.58 7.11C6.91 6.65 6 7.12 6 7.93v8.14c0 .81.91 1.28 1.58.82zM16 7v10c0 .55.45 1 1 1s1-.45 1-1V7c0-.55-.45-1-1-1s-1 .45-1 1z", "next");
-const Repeat = Svg("M7 7h10v1.79c0 .45.54.67.85.35l2.79-2.79c.2-.2.2-.51 0-.71l-2.79-2.79c-.31-.31-.85-.09-.85.36V5H6c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1s1-.45 1-1V7zm10 10H7v-1.79c0-.45-.54-.67-.85-.35l-2.79 2.79c-.2.2-.2.51 0 .71l2.79 2.79c.31.31.85.09.85-.36V19h11c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1s-1 .45-1 1v3z", "repeat");
-const Shuffle = Svg("M10.59 9.17L6.12 4.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.46 4.46 1.42-1.4zm4.76-4.32l1.19 1.19L4.7 17.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L17.96 7.46l1.19 1.19c.31.31.85.09.85-.36V4.5c0-.28-.22-.5-.5-.5h-3.79c-.45 0-.67.54-.36.85zm-.52 8.56l-1.41 1.41 3.13 3.13-1.2 1.2c-.31.31-.09.85.36.85h3.79c.28 0 .5-.22.5-.5v-3.79c0-.45-.54-.67-.85-.35l-1.19 1.19-3.13-3.14z", "shuffle");
-
-function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-    return (
-        <button
-            className={cl("button")}
-            {...props}
-        >
-            {props.children}
-        </button>
-    );
 }
 
 function CopyContextMenu({ name, type, path }: { type: string; name: string; path?: string; }) {
